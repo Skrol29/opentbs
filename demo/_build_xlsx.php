91f7560cb5f6be396216d4fbad14b8af0508e337
skrol29 <skrol29@91b6ad50-7c07-4dde-83ff-a8f70d914358>
2012-07-15 19:40:28 +0000

    OpenTBS: demo, manual, and few renaming

--- demo/_build_xlsx.php
+++ demo/_build_xlsx.php
@@ -13,8 +13,13 @@ $TBS->PlugIn(OPENTBS_SELECT_SHEET, 2);
 $TBS->MergeBlock('cell1,cell2', 'num', 3);
 $TBS->MergeBlock('b2', $data);
 
+// Merge pictures of the current sheet
+$x_picture = 'pic_1234f.png';
+$TBS->PlugIn(OPENTBS_MERGE_SPECIAL_ITEMS);
+
 // Delete a sheet
 $TBS->PlugIn(OPENTBS_DELETE_SHEETS, 'Delete me');
+
 
 // Display a sheet (make it visible)
 $TBS->PlugIn(OPENTBS_DISPLAY_SHEETS, 'Display me');
