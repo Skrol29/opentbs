06c65c8b29e02b7c22c94d8bf8db0b8b364aea3f
skrol29 <skrol29@91b6ad50-7c07-4dde-83ff-a8f70d914358>
2014-10-20 23:07:20 +0000

    OpenTBS: bug on the template download of the demo page

--- demo/demo_merge.php
+++ demo/demo_merge.php
@@ -1,21 +1,34 @@
 <?php
 
-// Display this code source if asked.
-if (isset($_GET['source'])) exit('<!DOCTYPE HTML><html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><title>OpenTBS plug-in for TinyButStrong - demo source</title></head><body>'.highlight_file(__FILE__,true).'</body></html>');
+// Display the asked source file.
+function f_source($file) {
+	echo '<!DOCTYPE HTML><html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><title>OpenTBS plug-in for TinyButStrong - demo source</title></head><body>'.highlight_file($file,true).'</body></html>';
+}
 
-// Read user choices
-if (!isset($_POST['btn_go'])) exit("You must use <a href='demo.html'>demo.html</a>");
 
 // Retrieve the template to open
 $template = (isset($_POST['tpl'])) ? $_POST['tpl'] : '';
 $template = basename($template); // for security
 $info= pathinfo($template);
+$script = $info['filename'].'.php';
 
 // Checks
 if (substr($template,0,5)!=='demo_') exit("Wrong file.");
 if (!file_exists($template)) exit("The asked template does not exist.");
 
-// Start the demo
-$script = $info['filename'].'.php';
-include($script);
 
+if (isset($_POST['btn_template'])) {
+	header('Location: '.$template); 
+	exit;
+}
+
+if (isset($_POST['btn_script'])) {
+	f_source($script);
+	exit;
+}
+
+// Start the demo
+if (isset($_POST['btn_result'])) {
+	include($script);
+	exit;
+}
