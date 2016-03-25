694d5c9fc060d75aba6d2b6be9c158a67871ce4f
skrol29 <skrol29forum+github@gmail.com>
2016-03-25 01:11:16 +0100

    OpenTBS 1.9.6 released

--- tbs_plugin_opentbs.php
+++ tbs_plugin_opentbs.php
@@ -7,8 +7,8 @@
  * This TBS plug-in can open a zip file, read the central directory,
  * and retrieve the content of a zipped file which is not compressed.
  *
- * @version 1.9.6-beta
- * @date 2016-03-10
+ * @version 1.9.6
+ * @date 2016-03-24
  * @see     http://www.tinybutstrong.com/plugins.php
  * @author  Skrol29 http://www.tinybutstrong.com/onlyyou.html
  * @license LGPL-3.0
@@ -91,7 +91,7 @@ class clsOpenTBS extends clsTbsZip {
 		if (!isset($TBS->OtbsClearMsPowerpoint))    $TBS->OtbsClearMsPowerpoint = true;
 		if (!isset($TBS->OtbsGarbageCollector))     $TBS->OtbsGarbageCollector = true;
 		if (!isset($TBS->OtbsMsExcelCompatibility)) $TBS->OtbsMsExcelCompatibility = true;
-		$this->Version = '1.9.6-beta';
+		$this->Version = '1.9.6';
 		$this->DebugLst = false; // deactivate the debug mode
 		$this->ExtInfo = false;
 		$TBS->TbsZip = &$this; // a shortcut
